e20e2d5724654e2d8eaaa4a7fd49dea8e186f92d
nrcfhub <nrcflab@gmail.com>
2025-04-24 05:43:03 -0700

    am a hockey player

---
+++ resume.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<title>
+	Beres resume
+</title>
+<link rel="stylesheet" type="text/css" href="resume-style.css">
+</head>
+<body>
+	<div class="profile-info-container">
+  <img src="profile.png" alt="Profile photo of Beres Muigai" class="profile-photo">
+  <div class="profile-info">
+    <h1>Beres Muigai</h1>
+    <p>Field: Medicine</p>
+    <p>Email: <a href="mailto:beresmuigai@gmail.com">beresmuigai@gmail.com</a></p>
+    <p>Phone: <a href="tel:+254748940630">0748940630</a></p>
+  </div>
+</div>
+<h2>my education and skills</h2>
+<table>
+  <tr>
+  <th> education</th>
+   <th>skills</th>
+   </tr>
+   <tr>
+    <td>art</td>
+    <td>drawing</td>
+   </tr>
+   <tr>
+    <td>web development</td>
+    <td>coding</td>
+  </tr>
+  <tr>
+    <td>hockey</td>
+    <td>dribbling</td>
+    </title>
+  </table>
+  <div>
+    <form action="send-message.php" method="post">
+      <label>
+        your name
+      </label>
+      <input type="text" name="sender">
+      <label>
+        message
+      </label>
+      <textarea name="message">
+      </textarea>
+      <button type="submit"> send
+      </button>
+    </form>
+  </div>
+</body>
+</html>
